762ecd87646f2da1ff38ef52e863adc77a46e3b9
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-08-09 00:23:27 +0100

    alx-interview -- minOps

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,19 +4,19 @@
 
 def minOperations(n):
     """
-	  function for the minOperation
-	  Gets fewest # of operations needed to result in exactly n H characters
-	"""
-    op, root = 0, 2
-    while root <= n:
-        # in n evely divides by root
+      function for the minOperation
+      Gets fewest # of operations needed to result in exactly n H characters
+    """
+    ops, root = 0, 2
+    if type(n) != int or n <= 1:
+        return 0
+    while n != 1:
         if n % root == 0:
-            # total even-division by tal operations
-            op += root
             # set n to the remainder
             n = n / root
-            # reduce root to find remaining smaller values that evenly-div n
-            root -= 1
+            ops = ops + root
+        else:
+            root = root + 1
         # increment root until it evely-divides n
         root += 1
-	return op
+	return ops
